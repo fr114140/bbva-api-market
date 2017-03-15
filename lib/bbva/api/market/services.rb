dede6d9ea523feb0a61fb56daa83acf89420261f
Enol Iglesias <enoliglesias@gmail.com>
2017-03-15 09:57:39 +0100

    Add payments path in services

--- lib/bbva/api/market/services.rb
+++ lib/bbva/api/market/services.rb
@@ -6,6 +6,7 @@ module Bbva
       IDENTITY_PATH   = "https://apis.bbva.com/identity-sbx/v1/me"
       CARDS_PATH      = "https://apis.bbva.com/cards-sbx/v1/me/cards"
       LOANS_PATH      = "https://apis.bbva.com/loans-sbx/v1/me"
+      PAYMENTS_PATH   = "https://apis.bbva.com/payments-sbx/v1/me"
       TOKEN_PATH      = "https://connect.bbva.com/token"
 
     end
