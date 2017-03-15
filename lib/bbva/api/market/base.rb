856b56bd4215f41732a95dda0a637c6f82fa61ec
Enol Iglesias <enoliglesias@gmail.com>
2017-03-15 10:22:37 +0100

    Add perform_post and otp method in order to allow 2FA

--- lib/bbva/api/market/base.rb
+++ lib/bbva/api/market/base.rb
@@ -3,6 +3,7 @@ module Bbva
   module Api
     module Market
       class Base
+
         attr_accessor :token, :secret, :code, :refresh_token, :client_id
 
         def initialize(options = {})
@@ -32,8 +33,31 @@ module Bbva
         private
 
           def perform_get url
-              response  = RestClient.get url, :content_type => "application/json", :Authorization => "jwt #{@token}"
-              JSON.parse(response)["data"].with_indifferent_access
+            response  = RestClient.get url, :content_type => "application/json", :Authorization => "jwt #{@token}"
+            JSON.parse(response)["data"].with_indifferent_access
+          end
+
+          def perform_post url, body = {}, token = nil
+            # We need to receive token param in order to use the Second Factor Authentication token when required
+            token ||= @token
+            response = RestClient.post(url, body.to_json, {:accept => "application/json",
+                                    :content_type => "application/json",
+                                    :Authorization => "jwt #{token}"})
+            JSON.parse(response)["data"].with_indifferent_access
+          end
+
+          def get_otp_url_and_token url, body = {}
+            # We need to use the &block in order to not raise exception of the 428 required for the 2FA
+            response = RestClient.post(url, body.to_json, {:accept => "application/json",
+                                    :content_type => "application/json",
+                                    :Authorization => "jwt #{@token}"}){|response, request, result| response }
+            parsed_response = JSON.parse(response)
+            if parsed_response["result"]["code"] == 428
+              data = parsed_response["data"]
+              ["#{data["otp_url"]}?ticket=#{data["ticket"]}&back_url=#{Settings.bbva.otp_back_url}&otp_token=#{data["token"]}", data["token"]]
+            else
+              raise "RestClient::RequestFailed Exception: HTTP status code #{parsed_response["result"]["code"]}"
+            end
           end
 
       end
