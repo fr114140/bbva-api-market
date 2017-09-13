e38dded6bdf8995ebfb2a42bb517613ed0fd0a5e
Daniel Romero <daniel.romero.esteban@gmail.com>
2017-09-13 12:29:22 +0200

    change urls to new api

--- lib/bbva/api/market/services.rb
+++ lib/bbva/api/market/services.rb
@@ -4,10 +4,10 @@ module Bbva
 
       ACCOUNTS_PATH   = "https://apis.bbva.com/accounts-sbx/v1/me/accounts"
       IDENTITY_PATH   = "https://apis.bbva.com/customers-sbx/v1/me-basic"
-      CARDS_PATH      = "https://apis.bbva.com/cards-sbx/v1/me/cards"
+      CARDS_PATH      = "https://apis.bbva.com/cards-sbx/v2/me/cards"
       LOANS_PATH      = "https://apis.bbva.com/loans-sbx/v1/me"
       PAYMENTS_PATH   = "https://apis.bbva.com/payments-sbx/v1/me"
-      PAYSTATS_PATH   = "https://apis.bbva.com/paystats_sbx/3"
+      PAYSTATS_PATH   = "https://apis.bbva.com/paystats_sbx/4"
       TOKEN_PATH      = "https://connect.bbva.com/token"
 
     end
--- lib/bbva/api/market/version.rb
+++ lib/bbva/api/market/version.rb
@@ -1,7 +1,7 @@
 module Bbva
   module Api
     module Market
-      VERSION = "0.2.1"
+      VERSION = "0.2.2"
     end
   end
 end
